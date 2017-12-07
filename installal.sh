949c323bc89999ce03a0a2af40b0aacd15c08d1f
whatisAI <clcaste@Camilos-MBP-2.connect>
2017-12-07 16:41:34 +0000

    m

--- installal.sh
+++ installal.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-#sudo apt-get update
-#sudo apt-get upgrade -y
-#sudo apt-get install git
-#git clone https://github.com/whatisAI/STL10
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt-get install git
 sudo apt-get install python3
-update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1
+sudo update-alternatives --install /usr/bin/python python /usr/bin/python3.5 1
 sudo apt-get install python3-pip
-pip3 install --update pip
-pip3 install --upgrade pip
-pip3 install numpy
-pip3 install matplotlib
-pip3 install tensorflow
-pip3 install keras
+sudo pip3 install pip
+sudo pip3 install --upgrade pip 
+sudo pip3 install numpy --upgrade 
+sudo pip3 install matplotlib  --upgrade  
+sudo pip3 install sklearn  --upgrade 
+sudo pip3 install pandas  --upgrade   
+sudo pip3 install tensorflow  --upgrade   
+#pip3 install --upgrade tensorflow-gpu
+sudo pip3 install keras --upgrade 
+#pip3 install networkx==1.11
 
+sudo apt-get install libhdf5
+sudo pip3 install h5py
 
-
-
-python download_data.py
